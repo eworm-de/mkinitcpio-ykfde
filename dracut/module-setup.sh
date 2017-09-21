e773b46301d40c977f2fb817571ec1c20c9fbace
Christian Hesse <mail@eworm.de>
2017-09-22 01:13:10 +0200

    ... same for dracut

--- dracut/module-setup.sh
+++ dracut/module-setup.sh
@@ -16,14 +16,14 @@ install() {
 	inst_simple "$moddir/ykfde.sh" /sbin/ykfde.sh
 	inst_simple /usr/lib/ykfde/worker
 	inst_simple /etc/ykfde.conf
+	inst_simple /usr/lib/systemd/system/ykfde-worker.service
+	ln_r $systemdsystemunitdir/ykfde-worker.service $systemdsystemunitdir/sysinit.target.wants/ykfde-worker.service
 
 	# this is required for second factor
 	if egrep -qi 'second factor = (yes|true|1)' /etc/ykfde.conf; then
 		inst_simple /usr/lib/systemd/system/cryptsetup-pre.target
 		inst_simple /usr/lib/systemd/system/ykfde-2f.service
 		ln_r $systemdsystemunitdir/ykfde-2f.service $systemdsystemunitdir/sysinit.target.wants/ykfde-2f.service
-		inst_simple /usr/lib/systemd/system/ykfde-worker.service
-		ln_r $systemdsystemunitdir/ykfde-worker.service $systemdsystemunitdir/sysinit.target.wants/ykfde-worker.service
 		inst_simple /usr/bin/systemd-ask-password
 		inst_simple /usr/bin/pkill
 		inst_simple /usr/bin/sleep
